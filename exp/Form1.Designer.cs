db7bdced411e7e4c79402551f1423cbaae597e18
diana <diatuaeva@gmail.com>
2019-05-08 11:09:05 +0300

    Добавьте файлы проекта.

---
+++ exp/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace exp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listBoxFiles = new System.Windows.Forms.ListBox();
+            this.buttonDirectory = new System.Windows.Forms.Button();
+            this.textBoxPath = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.listViewFiles = new System.Windows.Forms.ListView();
+            this.columnHeaderNameFile = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeaderSize = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeaderTypeFile = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBoxFiles
+            // 
+            this.listBoxFiles.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBoxFiles.FormattingEnabled = true;
+            this.listBoxFiles.ItemHeight = 31;
+            this.listBoxFiles.Location = new System.Drawing.Point(87, 120);
+            this.listBoxFiles.Name = "listBoxFiles";
+            this.listBoxFiles.Size = new System.Drawing.Size(246, 314);
+            this.listBoxFiles.TabIndex = 0;
+            // 
+            // buttonDirectory
+            // 
+            this.buttonDirectory.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonDirectory.Location = new System.Drawing.Point(358, 50);
+            this.buttonDirectory.Name = "buttonDirectory";
+            this.buttonDirectory.Size = new System.Drawing.Size(253, 38);
+            this.buttonDirectory.TabIndex = 1;
+            this.buttonDirectory.Text = "Обзор...";
+            this.buttonDirectory.UseVisualStyleBackColor = true;
+            this.buttonDirectory.Click += new System.EventHandler(this.buttonDirectory_Click);
+            // 
+            // textBoxPath
+            // 
+            this.textBoxPath.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxPath.Location = new System.Drawing.Point(87, 50);
+            this.textBoxPath.Name = "textBoxPath";
+            this.textBoxPath.Size = new System.Drawing.Size(246, 38);
+            this.textBoxPath.TabIndex = 2;
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.Description = "Выбрать папку для просмотра";
+            // 
+            // listViewFiles
+            // 
+            this.listViewFiles.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeaderNameFile,
+            this.columnHeaderSize,
+            this.columnHeaderTypeFile});
+            this.listViewFiles.LargeImageList = this.imageList1;
+            this.listViewFiles.Location = new System.Drawing.Point(373, 120);
+            this.listViewFiles.Name = "listViewFiles";
+            this.listViewFiles.Size = new System.Drawing.Size(299, 314);
+            this.listViewFiles.Sorting = System.Windows.Forms.SortOrder.Ascending;
+            this.listViewFiles.TabIndex = 3;
+            this.listViewFiles.UseCompatibleStateImageBehavior = false;
+            // 
+            // columnHeaderNameFile
+            // 
+            this.columnHeaderNameFile.Text = "Имя файла";
+            this.columnHeaderNameFile.Width = 103;
+            // 
+            // columnHeaderSize
+            // 
+            this.columnHeaderSize.Text = "Размер файла";
+            this.columnHeaderSize.Width = 104;
+            // 
+            // columnHeaderTypeFile
+            // 
+            this.columnHeaderTypeFile.Text = "тип файла";
+            this.columnHeaderTypeFile.Width = 89;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "prosto.png");
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(687, 38);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.listViewFiles);
+            this.Controls.Add(this.textBoxPath);
+            this.Controls.Add(this.buttonDirectory);
+            this.Controls.Add(this.listBoxFiles);
+            this.Name = "Form1";
+            this.Text = "Просмотр файлов в папке";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxFiles;
+        private System.Windows.Forms.Button buttonDirectory;
+        private System.Windows.Forms.TextBox textBoxPath;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.ListView listViewFiles;
+        private System.Windows.Forms.ColumnHeader columnHeaderNameFile;
+        private System.Windows.Forms.ColumnHeader columnHeaderSize;
+        private System.Windows.Forms.ColumnHeader columnHeaderTypeFile;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ exp/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace exp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonDirectory_Click(object sender, EventArgs e)
+        {
+
+            pictureBox1.Image = imageList1.Images[0];
+
+            string dir = "c:\\windows";
+            //string dir = @"c:\windows";
+            DirectoryInfo d = new DirectoryInfo(dir);
+
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                d = new DirectoryInfo(folderBrowserDialog1.SelectedPath);
+                textBoxPath.Text = d.FullName;
+                FileInfo[] files = d.GetFiles();
+                listBoxFiles.Items.Clear();
+                imageList1.ImageSize = new Size(32, 32);
+                foreach (var f in files)
+                {
+                    listBoxFiles.Items.Add(f.Name + " " + f.Length);
+                    //string[] s = new string[] { f.Name, f.Length.ToString() };
+                    //ListViewItem item = new ListViewItem(s);
+                    //listViewFiles.Items.Add(item);
+                    listViewFiles.Items.Add(new ListViewItem(new string[] { f.Name, f.Length.ToString(), f.Extension }));
+                    
+                    
+                }
+                this.listViewFiles.LargeImageList = imageList1;
+            }
+
+        }
+    }
+}
